fb34d01f990094a2cc0de516207a3163b7e5e722
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-09-30 15:13:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using oop_2024_09_30_shopping_cart.models;
+
+Console.WriteLine("Hello, World!");
+
+List<Fruit> fruits = new List<Fruit>()
+{
+
+    new Apple(), 
+    new Orange()
+};
+
+Cart cart = new Cart();
+    cart.AddFruit(fruits);
+Console.WriteLine($"Total cost is {cart.TotalCost()/100}");
---
+++ models/library.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2024_09_30_shopping_cart.models
+{
+    public static class CONSTANTS {
+
+        public const double COST_APPLE = .60;
+        public const double COST_ORANGE = .25;
+    }
+    public abstract class Fruit
+    {
+        public string Name { get; set; }
+        public double cost { get; set; }
+    }
+    public class Apple : Fruit
+    {
+        public Apple() { cost = CONSTANTS.COST_APPLE; }
+    }
+
+    public class Orange : Fruit
+    {
+        public Orange() { cost = CONSTANTS.COST_ORANGE; }
+    }
+
+    public class Cart
+    {
+        public List<Fruit> fruits { get; set; } = new();
+
+        public void AddFruit(List<Fruit> _listOfFruit)
+        {
+            fruits.AddRange(_listOfFruit);
+        }
+
+        public double TotalCost()
+        {
+            double totalCost = 0;
+            foreach (var fruit in fruits)
+            {
+                totalCost += fruit.cost;
+
+            }
+            return totalCost;
+        }
+
+    }
+}
